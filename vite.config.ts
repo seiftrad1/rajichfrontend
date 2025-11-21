7570edb1390bd556196e930a61758497e584d25f
seiftrad12 <strad@centurionco.com>
2025-11-21 15:11:23 +0100

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
-    base: "/rajichfrontend/",
+    base: "/MahdiaScout/",
 
     plugins: [react()],
 
